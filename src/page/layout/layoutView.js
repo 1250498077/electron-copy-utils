d0fe6bf23b1197591cc7e64b182403e0ee610f01
1250498077 <asd912345qq.com@qq.com>
2020-03-28 15:52:45 +0800

    完成基本功能开发

--- src/page/layout/layoutView.js
+++ src/page/layout/layoutView.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Component } from 'react';
 // import router from '../../router/router'
+import _ from 'lodash';
+// import {deepClone} from '../../utils'
 import './layoutScss.scss'
 import { Route, Switch, Link, withRouter } from 'react-router-dom';
-import { Input, Button, InputNumber } from 'antd';
+import { Input, Button, TreeSelect, Tree  } from 'antd';
 const fs = window.require('fs').promises
 const join = window.require('path').join;
-let level = 2;
-let currentlevel = 1;
+let filesList = [];
+let targetObj = {};
 let states = null;
+
+const { SHOW_PARENT } = TreeSelect;
 
 class LayoutView extends Component {
 
@@ -21,73 +25,75 @@ class LayoutView extends Component {
       fullScreen: false,
       files: '',
       // 设定搜索层级
-      level: 1
+      level: 1,
+      // 树
+      value: [],
+      // 树形数据
+      treeData: {
+        children: []
+      }
     }
+    this.currentlevel = 1;
   }
     
   componentDidMount = () => {
-    let filesList = this.geFileList("D:/react/新复制工具/my-app-new/src");
-    console.log('最终生成结果', filesList)
+    // let filesList = this.geFileList("D:/react/新复制工具/my-app-new/src");
+    // console.log('最终生成结果', filesList)
     // let str = JSON.stringify(filesList);
     // str = "var data ={name:'Egret',children:#1}".replace("#1",str);
     // this.writeFile("tree.js",str);
+    
   }
 
   geFileList = async (path) => {
-    let filesList = [];
-    let targetObj = {};
-
-    await this.readFile({
+    filesList = await this.readFile({
       path, filesList, targetObj
     });
     return filesList;
   }
 
   readFile = async ({
-    path, filesList, targetObj
+    path, 
   }) => {
-    let files = await fs.readdir(path);//需要用到同步读取
+    
+    let files = await fs.readdir(path);
+    let states = await fs.stat(path);
+    let fileNameArr = path.split('/');
+    let staticList = { 
+      name: path, 
+      type: 'folder',
+      size: states.size,
+      key: path,
+      value: path,
+      title: fileNameArr[fileNameArr.length-1],
+      children: []
+    };
+    // 遍历当前目录下的所有文件
+    for (let i = 0; i < files.length; i++) {
+      let fileName = files[i];
 
-    currentlevel++;
-    files.forEach(async (file) => {
-      states = await fs.stat(path + '/' + file);
-      if (states.isDirectory()) {
-        let item;
-        if (targetObj["children"]) {
-          item = { name: file, children: [] };
-          targetObj["children"].push(item);
-        }
-        else {
-          item = { name: file, children: [] };
-          filesList.push(item);
-        }
-
-        if (currentlevel <= this.state.level) {
-          this.readFile({
-            path: path + '/' + file, 
-            filesList, 
-            targetObj: item
-          });
-        }
-      } else {
-        //创建一个对象保存信息
-        let obj = new Object();
-        obj.size = states.size;//文件大小，以字节为单位
-        obj.name = file;//文件名
-        obj.path = path + '/' + file; //文件绝对路径
-
-        if (targetObj["children"]) {
-          let item = { name: file, value: obj.path }
-          targetObj["children"].push(item);
-        }
-        else {
-          let item = { name: file, value: obj.path };
-          filesList.push(item);
-        }
+      if (fileName === 'node_modules') {
+        return;
       }
-    });
-
-    return filesList;
+      let states = await fs.stat(path + '/' + fileName);
+      // 判断是不是文件夹
+      if (states.isDirectory()) {
+        let child = await this.readFile({
+          path: path + '/' + fileName
+        })
+        staticList.children.push(child);
+      } else {
+        staticList.children.push({
+          type: 'file',
+          size: states.size,
+          title: fileName,
+          key: path + '/' + fileName,
+          value: path + '/' + fileName,
+          isLeaf: true
+        })
+      }
+    }
+    return staticList;
   }
 
   writeFile(fileName,data) {  
@@ -96,6 +102,19 @@ class LayoutView extends Component {
        console.log("文件生成成功");
     }
   }
+
+  onChange = value => {
+    console.log('onChange ', value);
+    this.setState({ value });
+  };
+
+  onSelect = (selectedKeys, info) => {
+    console.log('selected', selectedKeys, info);
+  };
+
+  onCheck = (checkedKeys, info) => {
+    console.log('onCheck', checkedKeys, info);
+  };
 
   // 初始状态或状态变化会触发render
   render() {
@@ -108,7 +127,7 @@ class LayoutView extends Component {
               files: e.target.value
             })
           }} />
-          <InputNumber 
+          {/* <InputNumber 
             min={1}
             max={10} 
             defaultValue={3} 
@@ -117,20 +136,28 @@ class LayoutView extends Component {
                 level: value
               })
             }}
-          />
+          /> */}
           <Button
             type="primary"
-            onClick={() => {
+            onClick={async() => {
               const {files} = this.state;
               try {
                 let fileStr = files.replace(/\\/g, '/')
-                this.geFileList(fileStr);
+                let filesList = await this.geFileList(fileStr);
+                console.log('filesList filesList', filesList)
+                this.setState({ treeData:  filesList });
               } catch(e) {
-                console.log('出现问题')
+                console.log('读取目录出现问题')
               }
 
             }}>开始复制</Button>
         </div>
+        <Tree
+          checkable
+          onSelect={this.onSelect}
+          onCheck={this.onCheck}
+          treeData={this.state.treeData.children}
+        />
       </div>
     )
   }
